435e48fa3044dbe650388e55ea00ff764e3147dc
viktorliptakco <victor.liptak@gmail.com>
2023-03-24 15:32:30 +0100

    add TWO section content

--- js/script.js
+++ js/script.js
@@ -7,10 +7,37 @@ window.addEventListener('scroll', () => {
 
     navLinks.forEach(link => {
         link.classList.remove('active');
-        link.classList.remove(`class${link.getAttribute('href').slice(1)}`);
+        //link.classList.remove(`class${link.getAttribute('href').slice(1)}`);
     });
 
     const activeLink = navLinks[currentSectionIndex];
     activeLink.classList.add('active');
-    activeLink.classList.add(`class${activeLink.getAttribute('href').slice(1)}`);
+    //activeLink.classList.add(`class${activeLink.getAttribute('href').slice(1)}`);
 });
+
+
+
+
+
+
+const table = document.getElementById("myTable");
+const rows = table.rows;
+
+// Skryje všetky TD elementy
+for (let i = 0; i < rows.length; i++) {
+  rows[i].querySelector("td").style.display = "none";
+}
+
+// Pridá event listener na každý TH element
+for (let i = 0; i < rows.length; i++) {
+  const th = rows[i].querySelector("th");
+  th.addEventListener("click", function () {
+    // Skryje všetky TD elementy
+    for (let j = 0; j < rows.length; j++) {
+      rows[j].querySelector("td").style.display = "none";
+    }
+
+    // Zobrazí surodenca TD elementu kliknutého TH elementu
+    this.parentElement.querySelector("td").style.display = "table-cell";
+  });
+}
